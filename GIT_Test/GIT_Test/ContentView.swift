d109083ca044f33ed4b9fec17ad87a6ac6e9ca73
Dian DInihari | Tia <55896324+ddhyuliani@users.noreply.github.com>
2023-05-23 15:30:05 +0700

    Initial commit

---
+++ GIT_Test/GIT_Test/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  GIT_Test
+//
+//  Created by Dini on 23/05/23.
+//
+
+import SwiftUI
+
+// Orang ke 1
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+            }
+            .listStyle(.plain)
+            .navigationTitle("Awesome List")
+        }
+        
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct ListRowView: View {
+    var body: some View {
+        NavigationLink(destination: DetailView()) {
+            HStack(alignment: .top) {
+                Rectangle()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.blue)
+                
+                VStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: 80, height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                }
+                .foregroundColor(.orange)
+            }
+            .padding(.vertical, 10)
+        }
+    }
+}
+
---
+++ GIT_Test/GIT_Test/GIT_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_TestApp.swift
+//  GIT_Test
+//
+//  Created by Dini on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GIT_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
